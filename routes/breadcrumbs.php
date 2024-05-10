7b4d3daf79ecf7c34dba55f45a2ccbcd689b118c
Minami <kai3730421@gmail.com>
2024-05-10 17:13:33 +0900

    recipe-show-view1

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -10,11 +10,16 @@ use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
 
 // Home
 Breadcrumbs::for('home', function (BreadcrumbTrail $trail) {
-    $trail->push('Home', route('home'));
+  $trail->push('Home', route('home'));
 });
 
 // Home > レシピ一覧
 Breadcrumbs::for('index', function (BreadcrumbTrail $trail) {
   $trail->parent('home');
   $trail->push('レシピ一覧', route('recipe.index'));
+});
+
+Breadcrumbs::for('show', function (BreadcrumbTrail $trail, $recipe) {
+  $trail->parent('index');
+  $trail->push($recipe['title'], route('recipe.show', $recipe['id']));
 });

73487e8ed383d5c3a0bd8dc4c713bc59b6e01563
Minami <kai3730421@gmail.com>
2024-05-12 15:42:20 +0900

    review-post

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Review;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, $id)
+    {
+        $posts = $request->all();
+        // dd($posts, $id);
+
+        Review::insert([
+            'recipe_id' => $id,
+            'user_id' => $request->user()->id,
+            'rating' => $posts['rating'],
+            'comment' => $posts['comment']
+        ]);
+
+        return redirect()->route('recipe.show', ['id' => $id])
+            ->with('feedback.success', 'レビューを投稿しました!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
